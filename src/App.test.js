8330b38afff99ac8e1431844d5b35cb25d89ee39
Re9iNee <attarzadeh76@gmail.com>
2024-10-30 14:17:01 +0330

    m4

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { expect, it } from "vitest";
+import { describe } from "vitest";
+
+describe("Homepage", () => {
+  it("should render the homepage", () => {
+    expect(true).toBe(true);
+  });
+});
